37f6444436de83d0eeb976bd37c88100f78db3f8
Veronica Lee <veronicavlee@icloud.com>
2020-08-27 16:06:17 +0100

    Only print students with name starting with 'V'

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each_with_index do |student, i|
-    puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{i+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].upcase == "V"
   end
 end
 
