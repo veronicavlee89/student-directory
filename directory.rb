0f6236165bf9d6b86104e7b9ccffd1f5ad070b43
Veronica Lee <veronicavlee@icloud.com>
2020-08-27 17:04:33 +0100

    Center align all puts'ed text

--- directory.rb
+++ directory.rb
@@ -1,24 +1,27 @@
+@width = 160
+
 def input_students
-  puts "Please enter the information of the students"
-  puts "To finish, just hit return when prompted for a student name"
+  puts "Please enter the information of the students".center(@width)
+  puts "To finish, just hit return when prompted for a student name".center(@width)
+  puts ""
   # create an empty array
   students = []
   # get the first name
-  puts "Enter student's name"
+  puts "Enter student's name".center(@width)
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     height = '0'
     # get height that is numeric digits only and is > 0
     until /\A\d+\z/.match(height) && height.to_i > 0
-      puts "Enter #{name}'s height (in cm)"
+      puts "Enter #{name}'s height (in cm)".center(@width)
       height = gets.chomp
     end
     # add the student hash to the array
     students << {name: name, height: height, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(@width)
     # get another name from the user
-    puts "Enter next student's name"
+    puts "Enter next student's name".center(@width)
     name = gets.chomp
   end
   # return the array of students
@@ -26,22 +29,22 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(@width)
+  puts "-------------".center(@width)
 end
 
 def print(students)
   i = 0
   until i == students.length do
     if students[i][:name][0].upcase == "V" && students[i][:name].length < 12
-      puts "#{i+1}. #{students[i][:name]}, #{students[i][:height]}cm (#{students[i][:cohort]} cohort)"
+      puts "#{i+1}. #{students[i][:name]}, #{students[i][:height]}cm (#{students[i][:cohort]} cohort)".center(@width)
     end
     i += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(@width)
 end
 
 students = input_students
