42ae4c4e560923f18952b30aa4c0a72513bf8ab7
Veronica Lee <veronicavlee@icloud.com>
2020-08-27 23:31:27 +0100

    Refactor to use an instance variable of students

--- directory.rb
+++ directory.rb
@@ -1,5 +1,7 @@
 require 'date'
 
+@students = []
+
 ##
 # Puts's the passed in string, centered to a width of 160
 
@@ -11,9 +13,7 @@ def interactive_menu
   students = []
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
@@ -22,14 +22,32 @@ def interactive_menu
       students = input_students
     when "2"
       print_header
-      print(students)
-      print_footer(students)
+      print_students
+      print_footer
     when "9"
       exit # terminate program
     else
       puts "I don't know what you meant, try again"
     end
   end
+end
+
+##
+# Prints the interactive menu options
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+##
+# Prints the student information
+
+def show_students
+  print_header
+  print_students
+  print_footer
 end
 
 ##
@@ -43,17 +61,15 @@ def input_students
   log "Please enter the information of the students"
   log "To finish, just hit return when prompted for a student name"
   log "---"
-  students = []
   name = input_name
   while !name.empty? do
     height = input_height(name)
     cohort = input_cohort(name)
-    students << {name: name, height: height, cohort: cohort.downcase.to_sym}
-    log "Now we have #{students.count} #{students.count == 1 ?
+    @students << {name: name, height: height, cohort: cohort.downcase.to_sym}
+    log "Now we have #{@students.count} #{@students.count == 1 ?
                                            "student" : "students"}"
     name = input_name
   end
-  students
 end
 
 ##
@@ -105,10 +121,10 @@ end
 # Students are printed ordered by their cohort.
 # Information includes student name, height and cohort.
 
-def print(students)
-  return if students.count == 0
-  students.sort_by! { |student| Date::MONTHNAMES.index(student[:cohort].to_s.capitalize) }
-  students.each_with_index { |student, i|
+def print_students
+  return if @students.count == 0
+  @students.sort_by! { |student| Date::MONTHNAMES.index(student[:cohort].to_s.capitalize) }
+  @students.each_with_index { |student, i|
     if student[:name][0].upcase == "V" && student[:name].length < 12
       log "#{i+1}. #{student[:name]}, #{student[:height]}cm "\
           "(#{student[:cohort].capitalize} cohort)"
@@ -120,12 +136,12 @@ end
 # Prints a footer for the Villains Academy student list, including a count of
 # students.
 
-def print_footer(students)
-  if students.count == 0
+def print_footer
+  if @students.count == 0
     log "There are no students"
     return
   end
-  log "Overall, we have #{students.count} great #{students.count == 1 ?
+  log "Overall, we have #{@students.count} great #{@students.count == 1 ?
                                                     "student" : "students"}"
 end
 
