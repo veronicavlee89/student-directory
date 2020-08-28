264c418077cc56310136a8a77ec39d1a866ae681
Veronica Lee <veronicavlee@icloud.com>
2020-08-28 14:33:55 +0100

    Add option to save students to file, including name and cohort

--- directory.rb
+++ directory.rb
@@ -18,6 +18,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -30,6 +31,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -60,6 +63,19 @@ def input_students
                                            "student" : "students"}"
     name = input_name
   end
+end
+
+##
+# Saves the inputted student information to a file
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 ##
